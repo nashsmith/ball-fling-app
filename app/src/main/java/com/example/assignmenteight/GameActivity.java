e69c3a0a28593d6e876ae2e9e31b9476b629de11
Govender, Justin <jmg66@students.waikato.ac.nz>
2019-09-30 23:39:58 +1300

    Added the ability to fling the ball.

--- app/src/main/java/com/example/assignmenteight/GameActivity.java
+++ app/src/main/java/com/example/assignmenteight/GameActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
@@ -25,12 +27,13 @@ public class GameActivity extends AppCompatActivity {
     //Ball speed
     float dx = 0;
     float dy = 0;
+    //Ball options
+    boolean isFlingable = true;
     //Ball parameters
-    static final int BALL_RADIUS = 70;
-    static final int BALL_MASS = 8;
+    static final int BALL_RADIUS = 35;
+    static final int BALL_MASS = 256;
     static final int BALL_TERMINAL_VELOCITY = 20;
     static final double BOUNCINESS = 0.6;
-    String s = "hello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,25 +65,72 @@ public class GameActivity extends AppCompatActivity {
         constraintLayout.addView(graphicsView);
     }
 
+    public void onclickButtonReset(View view) {
+        recreate();
+    }
+
+
     //Custom View for drawing the ball
     public class GraphicsView extends View {
         //Declare variables
         Paint paint = new Paint();
+        GestureDetector gestureDetector;
         //Constructor
         public GraphicsView(Context context) {
             super(context);
+            //Create gesture detector
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onDown(MotionEvent motionEvent) {
+                    return true;
+                }
+
+                @Override
+                public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float velocityX, float velocityY) {
+                    dx = velocityX/BALL_MASS;
+                    dy = velocityY/BALL_MASS;
+                    isFlingable = false;
+                    return true;
+                }
+            });
             //Set color of the ball
             paint.setColor(getColor(R.color.colorPrimary));
             //Set initial position to the centre of the screen
             x = width/2;
-            y = height/2;
+            y = height - 50;
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
+            //If the ball will still be on the screen...
+            if(x + dx > BALL_RADIUS && x + dx < width - BALL_RADIUS)
+                x = x + dx; //Move the ball
+            else //Else if we are hitting a wall, bounce!
+                dx = (float)(-dx*BOUNCINESS);
+            if(y + dy > BALL_RADIUS && y + dy < height - BALL_RADIUS)
+                y = y + dy;
+            else
+                dy = (float)(-dy*BOUNCINESS);
+            //Draw the ball in the new position
             canvas.drawCircle(x, y,BALL_RADIUS,paint);
             //Redraw in the next position
             invalidate();
         }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if(isFlingable){
+                if(event.getX() > BALL_RADIUS && event.getX() < width - BALL_RADIUS)
+                    x = event.getX();
+                if(event.getY() > BALL_RADIUS && event.getY() < height - BALL_RADIUS)
+                    y = event.getY();
+                if(gestureDetector.onTouchEvent(event)){
+                    return true;
+                }
+                return super.onTouchEvent(event);
+            }
+            return true;
+        }
+
     }
 }
